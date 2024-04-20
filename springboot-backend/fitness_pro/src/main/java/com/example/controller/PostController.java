4f95d137cb7cfd3bb5b856961349e3b9ad433d53
Thisera K.G <99192717+it21204166@users.noreply.github.com>
2024-04-20 20:53:42 +0530

    added get mapping method using id

--- springboot-backend/fitness_pro/src/main/java/com/example/controller/PostController.java
+++ springboot-backend/fitness_pro/src/main/java/com/example/controller/PostController.java
@@ -3,12 +3,15 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.exception.ResourceNotFoundException;
 import com.example.model.Post;
 import com.example.repository.PostRepository;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +34,13 @@ public class PostController {
     @PostMapping("/posts")
     public Post createPost(@RequestBody Post post){
         return postRepository.save(post);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id ){
+        Post post = postRepository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Post not exist with id :"+ id));
+        return ResponseEntity.ok(post);
     }
 
 }
