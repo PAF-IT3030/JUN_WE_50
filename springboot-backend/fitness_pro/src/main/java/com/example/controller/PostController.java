505e4826a9ff77585bf1c515f91d6a6fd864252f
Thisera K.G <99192717+it21204166@users.noreply.github.com>
2024-04-20 20:44:40 +0530

    added get mapping method

--- springboot-backend/fitness_pro/src/main/java/com/example/controller/PostController.java
+++ springboot-backend/fitness_pro/src/main/java/com/example/controller/PostController.java
@@ -1,5 +1,33 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.Post;
+import com.example.repository.PostRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
 public class PostController {
     
+    @Autowired
+	private PostRepository postRepository;
+
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+        return postRepository.findAll();
+    }
+
+
+    
+
 }
