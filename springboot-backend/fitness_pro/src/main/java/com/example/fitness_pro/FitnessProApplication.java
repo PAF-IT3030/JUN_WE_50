ee118e8582819a0903fab7136ca91ad94b9b867b
Thisera K.G <99192717+it21204166@users.noreply.github.com>
2024-04-20 19:54:32 +0530

    frontend added and backend added

---
+++ springboot-backend/fitness_pro/src/main/java/com/example/fitness_pro/FitnessProApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitness_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessProApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/fitness_pro/src/test/java/com/example/fitness_pro/FitnessProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitness_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
