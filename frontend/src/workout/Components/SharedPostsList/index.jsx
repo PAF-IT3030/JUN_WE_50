07398b3bdca9124896a2eb44a8947face24b0dbf
IT21282454 <it21282454@my.sliit.lk>
2024-05-07 14:10:53 +0530

    update post share component

--- frontend/src/workout/Components/SharedPostsList/index.jsx
+++ frontend/src/workout/Components/SharedPostsList/index.jsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
 import SharedPostCard from "../SharedPostCard";
 
-function SharedPostsList({posts, fetchType}) {
-  const [postsList, setPostsList] = useState([]);
+function SharedPostsList({ posts, fetchType }) {
+    const [postsList, setPostsList] = useState([]);
 
-  useEffect(()=>{
-    if(posts){
-      setPostsList(posts);
-    }
-  },[posts]);
+    useEffect(() => {
+        if (posts) {
+            setPostsList(posts);
+        }
+    }, [posts]);
 
-  return (
-    <div>
-        {postsList.length ? [...postsList].reverse().map((post) => {
-          return <SharedPostCard key={post.id} post={post} fetchType={fetchType}/>;
-        }) : <h5>No shared posts yet...</h5>}
-    </div>
-  );
+    return (
+        <div>
+            {postsList.length ? (
+                [...postsList].reverse().map((post) => {
+                    return (
+                        <SharedPostCard
+                            key={post.id}
+                            post={post}
+                            fetchType={fetchType}
+                        />
+                    );
+                })
+            ) : (
+                <h5>No shared posts yet...</h5>
+            )}
+        </div>
+    );
 }
 
 export default SharedPostsList;
