c1a74b2458e19c6f9e22e9404aa579833ade1ecd
IT21282454 <it21282454@my.sliit.lk>
2024-05-07 14:16:10 +0530

    update shared post

--- frontend/src/workout/Pages/SharedPosts/index.jsx
+++ frontend/src/workout/Pages/SharedPosts/index.jsx
@@ -5,20 +5,23 @@ import { getPostShareByUserId } from "../../app/actions/postshare.actions";
 import SharedPostsList from "../../Components/SharedPostsList";
 
 function SharedPosts() {
-  const dispatch = useDispatch();
-  const post = useSelector((state) => state.postshare);
-  const comment = useSelector((state) => state.comment);
-  const user = useSelector((state) => state.user);
-  useEffect(() => {
-    if (user.userId) {
-      dispatch(getPostShareByUserId(user.userId));
-    }
-  }, [dispatch, user.userId]);
-  return (
-    <div>
-        <SharedPostsList posts={post.posts} fetchType="GET_ALL_POSTS_SHARED"/>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const post = useSelector((state) => state.postshare);
+    const comment = useSelector((state) => state.comment);
+    const user = useSelector((state) => state.user);
+    useEffect(() => {
+        if (user.userId) {
+            dispatch(getPostShareByUserId(user.userId));
+        }
+    }, [dispatch, user.userId]);
+    return (
+        <div>
+            <SharedPostsList
+                posts={post.posts}
+                fetchType="GET_ALL_POSTS_SHARED"
+            />
+        </div>
+    );
 }
 
 export default SharedPosts;
